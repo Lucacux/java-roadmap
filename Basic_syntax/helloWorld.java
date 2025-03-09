dd017e512e32e3d00f1c6a73ba7f67b80939f249
Luca Lombardo <lucalombardi336@gmail.com>
2025-03-09 18:40:54 -0300

    Created directory for Strings and Methods exercises in the SH roadmap

--- Basic_syntax/helloWorld.java
+++ Basic_syntax/helloWorld.java
@@ -1,6 +1,7 @@
 // Libraries are imported.
 import java.util.Scanner; // Scanner class is imported.
 
+@SuppressWarnings("unused")
 public class helloWorld {
   public static void main (String[] args) {
 
